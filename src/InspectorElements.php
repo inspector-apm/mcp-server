7dd3d404eec2c182649db18bf7eeb802c49ac88d
Valerio Barbera <valerio@inspector.dev>
2025-08-19 13:15:01 +0200

    errors report

--- src/InspectorElements.php
+++ src/InspectorElements.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Inspector\MCPServer;
 
-use GuzzleHttp\Exception\GuzzleException;
 use Inspector\MCPServer\Reports\ErrorsListReport;
 use PhpMcp\Server\Attributes\McpTool;
 
